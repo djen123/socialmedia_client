46d2674ea29d024e1a5fb80b2b5d5b6c7f7d9d41
djen123 <djen_6@hotmail.com>
2026-09-20 10:13:15 +0100

    add

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
  // baseURL: "http://localhost:3000/api",
- baseURL:`${import.meta.env.VITE_API_URL}`,
+ baseURL:`${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json"
